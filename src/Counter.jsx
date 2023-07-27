f2dd3f49599b2fc615ca2b38103e76f093a26669
HenokG <heni.gebrehiwot@gmail.com>
2023-07-27 17:13:51 -0600

    Section 4 - Game Status and Last Play

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -8,6 +8,8 @@ export default class Counter extends React.Component {
         this.handleDefence = this.handleDefence.bind(this);
         this.state = {
           count: 0,
+          gameStatus: "",
+          lastPlay: "",
         };
       }
     handleAttack= () => {
@@ -15,6 +17,8 @@ export default class Counter extends React.Component {
             let newCount = previousState.count + Math.round(Math.random() * 10);
             return {
               count: newCount,
+              lastPlay: "Attack",
+              gameStatus: newCount > 10 ? "You Won!!" : previousState.gameStatus,
             };
           });
       };
@@ -24,6 +28,8 @@ export default class Counter extends React.Component {
             let newCount = previousState.count - Math.round(Math.random() * 10);
             return {
               count: newCount,
+              lastPlay: "Defence",
+              gameStatus: newCount < -10 ? "You Lost!!" : previousState.gameStatus,
             };
           });
 
@@ -40,6 +46,8 @@ export default class Counter extends React.Component {
         this.setState(() => {
           return {
             count: 0,
+            gameStatus: "",
+            lastPlay: "",
           };
         });
       };
@@ -48,9 +56,9 @@ export default class Counter extends React.Component {
         <div className="row text-white text-center">
         <h1>Game Score: {this.state.count} </h1>
         <p>You win at +10 points and lose at -10 points!</p>
-        <p>Last Play: </p>
-        <h3>Game Status : </h3>
-        <div className="col-6 col-md-3 offset-md-3">
+        <p>Last Play:{this.state.lastPlay} </p>
+        <h3>Game Status :{this.state.gameStatus} </h3>
+        <div className="col-6 col-md-3">
           <img
             style={{
               width: "100%",
